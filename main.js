ff7040473e721534d08fff50c9a94b6e3aeceb02
Xenock20 <101891933+Xenock20@users.noreply.github.com>
2022-08-22 18:16:18 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+let altura = 190;
+
+console.log(altura);
+
+let alturaMetros = 1.90;
+
+let peso = 40.7;
+
+let alturaMetrosR =  Math.round(alturaMetros);
+
+console.log(alturaMetrosR);
+
+let pesoR = Math.floor(peso);
+
+console.log(pesoR);
+
+const sonIguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
